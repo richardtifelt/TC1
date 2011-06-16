f8a57d535908ff9546091947792a14e594c751f0
Richard Johansson <richard@newsdesk.se>
2011-06-16 13:22:39 +0200

    Registered prod Twitter app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.configuration.middleware.use OmniAuth::Builder do
-  provider :twitter, "1V9lty4RBVlA0E5TObJFw", "IBZgFxmzVQltxhg4wIYhv4F4IHrdvMhLdBplSvuj5lc", {:callback_path => "/auth_twitter"}
+  if Rails.env.development?
+    provider :twitter, "1V9lty4RBVlA0E5TObJFw", "IBZgFxmzVQltxhg4wIYhv4F4IHrdvMhLdBplSvuj5lc", {:callback_path => "/auth_twitter"}
+  else
+    provider :twitter, "RW4KqKih7fkw9N59GpffLg", "KbEsl5rBJSFbIgMDgM0zXuvnN7qoJDPENe9DK2pZhAY", {:callback_path => "/auth_twitter"}
+  end
 end
