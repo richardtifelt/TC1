d31c7fe91d043107e225abe65ac9d10f063c415f
Joakim Westerlund <joakim@newsdesk.se>
2011-06-17 13:11:14 +0200

    fixed size of text depending on the length of joke

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def jokes_class_name str
+    class_name = "jokes"
+    if  str.length < 150
+      class_name = "jokes-short"
+    end
+    return class_name
+  end
 end
